091531a67648a493c9115a05a3d0782f4a05c920
lyc <1174510490@qq.com>
2020-05-27 19:10:03 +0800

    签单功能完成

--- vc/tarn.forum.vc/assets/utils/route.js
+++
@@ -1,7 +0,0 @@
-// 获取url地址中参数的值
-export const getUrlString = key => {
-  const reg = new RegExp("(\\?|&)" + key + "=([^&]*)(&|$)");
-  const r = window.location.href.match(reg);
-  return r ? window.decodeURI(r[2]).replace(/#.*$/, "") : "";
-};
-
